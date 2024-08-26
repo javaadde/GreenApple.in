eaa4776ab2cf663b80ff5c6ca7be383f99d8a165
javad <iamkuttappi@gmail.com>
2024-08-26 11:06:14 +0530

    committing desingn-2 for the 404 error

---
+++ script.js
@@ -0,0 +1,11 @@
+ function openNev(){
+
+    document.getElementById("nav").style.display="flex"
+    document.getElementById("icon-mine").style.display="none"
+
+ }
+ function closeNev(){
+    document.getElementById("nav").style.display="none"
+    document.getElementById("icon-mine").style.display="flex"
+
+ }
